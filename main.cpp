48c713fa769f9a917f020e286d6a8f3fb0abf18e
u211-15 <u211-15@public.mpei.local>
2019-04-01 10:52:17 +0300

    new

--- main.cpp
+++ main.cpp
@@ -2,28 +2,19 @@
 #include <vector>
 
 using namespace std;
-
-int
-main() {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
+vector<double>
+input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
     }
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
+    return result;
+}
+void
+find_minmax(vector<double> numbers, double& min, double& max) {
+    min = numbers[0];
+    max=numbers[0];
+        for (double number : numbers) {
         if (number < min) {
             min = number;
         }
@@ -31,8 +22,12 @@ main() {
             max = number;
         }
     }
+}
+ void
+ make_histogram(vector<size_t>& bins,vector <double> numbers, size_t &bin_count) {
+     double min, max;
+find_minmax (numbers, min, max);
 
-    vector<size_t> bins(bin_count);
     for (double number : numbers) {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
         if (bin == bin_count) {
@@ -40,13 +35,14 @@ main() {
         }
         bins[bin]++;
     }
-
-    // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
+ }
+ void
+show_histogram_text(vector <size_t> bins) {
+     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
     size_t max_count = 0;
-    for (size_t count : bins) {
+    for ( size_t count : bins) {
         if (count > max_count) {
             max_count = count;
         }
@@ -73,6 +69,32 @@ main() {
         }
         cout << '\n';
     }
+ }
+
+int
+main() {
+
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+
+
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+
+
+
+    vector<size_t> bins(bin_count);
+  make_histogram (bins, numbers, bin_count);
+
+  show_histogram_text(bins);
+
 
     return 0;
 }
